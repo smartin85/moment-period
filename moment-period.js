8a83a2a90f88a3988a9e3fc308473e1a7ade2f41
Smartin <smartin.negele@gmail.com>
2017-02-13 20:35:24 +0100

    Updated version

--- moment-period.js
+++ moment-period.js
@@ -11,7 +11,7 @@
 }(this, function (moment) {
 	"use strict";
 
-    var VERSION = "1.0.3",
+    var VERSION = "1.0.4",
         periods = {},
         periodFlag = '_p',
         optionsFlag = '_po',
