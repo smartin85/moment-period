677ec800c766aafa49ffebcafc6bd5014b4188d5
Smartin <smartin@nematronic.de>
2021-02-14 10:19:11 +0100

    removed gulp and bower

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,14 @@
 var package = require('./package.json');
 
 module.exports = function (config) {
+  const testTarget = `moment-period${process.env.MINIFIED_TESTS ? '.min' : ''}.js`;
+  console.log(`Testing ${testTarget} ...`);
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
     files: [
       'node_modules/moment/moment.js',
-      'moment-period.js',
+      testTarget,
       'tests/moment-period.spec.js',
       'tests/*.spec.js',
     ],
